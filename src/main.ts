7666376f3d11aff922371896154482171c0be459
xwarockx3 <xwarockx3@gmail.com>
2021-03-16 16:39:38 +0100

    Co-authored-by: TecCheck <TecCheck@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@mdi/font/css/materialdesignicons.css'
+
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false
 
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi', // default - only for display purposes
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#47c8b9"
+      }
+    }
+  }
+})
+
 new Vue({
   router,
+  vuetify: new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-app>
+    <v-card class="mx-auto" max-width="344" outlined>
+      <v-card-text three-line>
+
+        <v-text-field 
+          outlined
+          dense 
+          label="Benutzername"
+          clearable>
+        </v-text-field>
+
+        <v-text-field 
+          outlined
+          dense
+          label="Passwort"
+          clearable>
+        </v-text-field>
+
+        <v-checkbox
+          v-model="ex4"
+          label="Logindaten merken"
+          color="primary">
+        </v-checkbox>
+
+        <v-btn
+          color="primary"
+          outlined>
+        Login
+        </v-btn>
+      </v-card-text>      
+    </v-card>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Login',
+  components: {},
+});
+</script>
