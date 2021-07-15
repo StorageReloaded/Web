ffedd0a2e35e9cba72bb0e86e7b48c9e7379378f
TecCheck <mcteccy@protonmail.com>
2021-07-15 22:30:25 +0200

    Add Roboto font

--- src/App.vue
+++ src/App.vue
@@ -23,12 +23,14 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap');
+
 html {
   overflow: hidden !important;
 }
 
 #app {
-  font-family: Roboto, Avenir, Helvetica, Arial, sans-serif;
+  font-family: Roboto, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
