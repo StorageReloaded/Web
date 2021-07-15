6de3e9c732afa4563389ac01dfcd7bc42fc7654a
TecCheck <mcteccy@protonmail.com>
2021-07-15 16:00:40 +0200

    Create simple about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-app>
+    <!-- TODO: could be better -->
+    <app-drawer></app-drawer>
+
+    <v-container fill-height fluid>
+      <div class="mx-auto" width="600" outlined style="padding: 32px">
+        <v-img
+          :src="require('@/assets/banner.svg')"
+          style="margin: 16px 64px"
+        ></v-img>
+
+        <v-card-text class="text-center" style="font-size: 14pt">
+          <p >Simple self-hosted/all-in-one storage management system</p>
+          <a href="https://github.com/StorageReloaded" target="_blank">GitHub</a>
+        </v-card-text>
+      </div>
+    </v-container>
+  </v-app>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import AppDrawer from "@/components/AppDrawer.vue";
+
+@Component({ components: { AppDrawer } })
+export default class About extends Vue {}
+</script>
